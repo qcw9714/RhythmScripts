ff217b120a0c90a6b1d18b5bc62c2f316c4d3ad5
fangy14 <fangy14@mails.tsinghua.edu.cn>
2016-12-27 20:45:16 +0800

    Add: Gong

---
+++ GongSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class GongSound : AbstractSound {
+	
+	public GongSound() {
+	}
+
+	override public bool UpdateFrame (ref Frame frame) {
+		HandList hands = frame.Hands;
+		Hand firstHand = hands [0];
+		bool isTrue = false;
+		float recentSphereRadius = firstHand.SphereRadius;
+		if (recentSphereRadius > 0) {
+			if (lastSphereRadius > drumThreshold && recentSphereRadius <= drumThreshold) {
+				Debug.Log (firstHand.SphereRadius);
+				AudioClip drum = GameObject.Find("Hand Controller").GetComponent<AC>().drum;
+				Vector3 position = GameObject.Find ("Hand Controller").GetComponent<AC> ().position;
+				AudioSource.PlayClipAtPoint (drum,position);
+
+				isTrue = true;
+			}
+			lastSphereRadius = recentSphereRadius;
+		}
+		return isTrue;
+	}
+}
