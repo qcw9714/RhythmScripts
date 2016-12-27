74a77dab0d5834d0460f2d2da0d84a151bf0e53c
qcw <qcw14@mails.tsinghua.edu.cn>
2016-12-28 01:35:12 +0800

    gongsoundfinish

--- GongSound.cs
+++ GongSound.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 using Leap;
 
 public class GongSound : AbstractSound {
+
+	public int Index_Now = 0;
 	static readonly float FingerStrightState_Radian = Mathf.PI/12;//15度
 	public bool isDown = false;//手掌是不是已经向下运动
 	public Vector[] RightPosition;//指尖位置
 	public Vector[] RightDirection;//指尖方向
 	public Vector[] RightRoot;//根骨位置
-	public Vector PalmPosition;//手掌掌心位置
+	public Vector[] PalmPosition;//手掌掌心位置以及上一帧手掌掌心位置
 	public Vector PalmNormal;//手掌法向量
 	public Vector PalmVelocity;//手掌运动方向
 	public bool[] IsAllStraight = new bool[5];
-	public bool isMiddleStraight;
+	public bool isMiddleStraight = false;
 	bool isHandDown = false;
 	bool isMoveDown = false;
 	bool isMoveUp = false;
 	public GongSound() {
 		RightPosition  = new Vector[5];
 		RightDirection  = new Vector[5];
-		PalmPosition = new Vector (0, 0, 0);
+		PalmPosition = new Vector[2];
 		PalmNormal = new Vector (0, 0, 0);
 		PalmVelocity = new Vector (0, 0, 0);
 		RightRoot = new Vector[5];
@@ -29,9 +31,12 @@ public class GongSound : AbstractSound {
 			RightDirection [j] = new Vector (0, 0, 0);
 			RightRoot [j] = new Vector (0, 0, 0);
 		}
+		for (int i = 0; i < 2; i++) {
+			PalmPosition [i] = new Vector (0, 0, 0);
+		}
 	}
 	public void MakeAllZero(){
-		PalmPosition = Vector.Zero;
+		PalmPosition[Index_Now] = Vector.Zero;
 		PalmNormal = Vector.Zero;
 		PalmVelocity = Vector.Zero;
 		for (int j = 0; j < 5; j++) {
@@ -91,6 +96,13 @@ public class GongSound : AbstractSound {
 			}
 		}
 	}
+	double CalDis(Vector v,Vector e){
+		double temp0 = System.Math.Pow((double)v.x-(double)e.x,2.0);
+		double temp1 = System.Math.Pow((double)v.y-(double)e.y,2.0);
+		double temp2 = System.Math.Pow((double)v.z-(double)e.z,2.0);
+		double res = System.Math.Sqrt (temp0 + temp1 + temp2);
+		return res;
+	}
 	override public bool UpdateFrame (ref Frame frame) {
 		bool isTrue = false;
 		HandList hands = frame.Hands;
@@ -99,7 +111,7 @@ public class GongSound : AbstractSound {
 			MakeAllZero ();
 		} else if (count == 1) {
 			if (hands [0].IsRight) {//场景中有右手
-				PalmPosition = hands [0].PalmPosition;
+				PalmPosition[Index_Now] = hands [0].PalmPosition;
 				PalmNormal = hands [0].PalmNormal;
 				PalmVelocity = hands [0].PalmVelocity;
 				SetData (hands [0]);
@@ -113,7 +125,7 @@ public class GongSound : AbstractSound {
 			} else {
 				hand = hands [1];
 			}//hand为右手
-			PalmPosition = hand.PalmPosition;
+			PalmPosition[Index_Now] = hand.PalmPosition;
 			PalmNormal = hand.PalmNormal;
 			PalmVelocity = hand.PalmVelocity;
 			SetData(hand);
@@ -130,8 +142,8 @@ public class GongSound : AbstractSound {
 				Sum = Sum + 1;//伸直手指总数
 			}
 		}
-		//然后看手是不是向下的,如果手掌法向量与（0,-1,0）夹角小于15度则是向下
-		float f = Mathf.PI/12;//15度
+		//然后看手是不是向下的,如果手掌法向量与（0,-1,0）夹角小于10度则是向下
+		float f = Mathf.PI/24;//15度
 		if (!PalmNormal.Equals (Vector.Zero)) {
 			float radian = PalmNormal.AngleTo (new Vector (0, -1, 0));
 			if (radian < f) {
@@ -139,8 +151,8 @@ public class GongSound : AbstractSound {
 			}
 		}
 			
-		//然后计算手掌当前运动方向与（0,-1,0）夹角，小于15度认为是向下运动
-		if (!PalmVelocity.Equals (Vector.Zero)) {
+		//然后计算手掌当前运动方向与（0,-1,0）夹角，小于5度认为是向下运动
+	/*	if (!PalmVelocity.Equals (Vector.Zero)) {
 			float radian = PalmVelocity.AngleTo (new Vector (0, -1, 0));
 			if (radian < f) {
 				isMoveDown = true;
@@ -152,16 +164,36 @@ public class GongSound : AbstractSound {
 			if (radian < f) {
 				isMoveUp = true;
 			}
-		}
+		}*/
 		//还需要限制移动速度
-		double Downgg =400.0;
+		/*double Downgg =400.0;
 		double Upgg = 300.0;
 		double temp0 = System.Math.Pow((double)PalmVelocity.x,2.0);
 		double temp1 = System.Math.Pow((double)PalmVelocity.y,2.0);
 		double temp2 = System.Math.Pow((double)PalmVelocity.z,2.0);
 		double res = System.Math.Sqrt (temp0 + temp1 + temp2); 
-		Debug.Log (res);
-		if (isHandDown && isMoveDown && Sum >= 3 && isMiddleStraight && !isDown && res>=Downgg) {
+		//Debug.Log (res);
+		Debug.Log("速度：");
+		Debug.Log (PalmVelocity.Normalized);*/
+		//不通过手掌速度判断了，而是通过两帧之间的不用，需要缓存上一帧的PalmPosition
+		double dis = 0.0;
+		dis = CalDis (PalmPosition[Index_Now],PalmPosition[(Index_Now+1)%2]);//移动距离
+		Vector vec = PalmPosition[Index_Now] - PalmPosition[(Index_Now+1)%2];//移动向量
+		if (!vec.Equals (Vector.Zero)) {
+			float radian = vec.AngleTo (new Vector (0, -1, 0));
+			if (radian < f) {
+				isMoveDown = true;
+			}
+		}
+
+		if (!vec.Equals (Vector.Zero)) {
+			float radian = vec.AngleTo (new Vector (0, 1, 0));
+			if (radian < f) {
+				isMoveUp = true;
+			}
+		}
+
+		if (isHandDown && isMoveDown && PalmPosition[Index_Now].y < PalmPosition[(Index_Now+1)%2].y - 5.0 && Sum >= 3 && isMiddleStraight && !isDown) {
 			isDown = true;
 			AudioClip drum = GameObject.Find("Hand Controller").GetComponent<AC>().gong;
 			Vector3 position = GameObject.Find ("Hand Controller").GetComponent<AC> ().position;
@@ -169,11 +201,11 @@ public class GongSound : AbstractSound {
 			isTrue = true;
 		}
 
-		if (isHandDown && isMoveUp && Sum >= 3 && isMiddleStraight && res>=Upgg) {
+		if (isHandDown  && isMoveUp && PalmPosition[Index_Now].y > PalmPosition[(Index_Now+1)%2].y + 5.0 && Sum >= 3 && isMiddleStraight && isDown) {
 			isDown = false;
 		}
-		//isDown恢复
 
+		Index_Now = (Index_Now + 1) % 2;
 		return isTrue;
 	}
 }
